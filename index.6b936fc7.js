7f2dc37a98656cbc32cb1608540ed61326c6746b
VolodymyrKolisnichenko <90705859+vk-workshop@users.noreply.github.com>
2024-05-10 19:16:55 +0200

    make build

---
+++ index.6b936fc7.js
@@ -0,0 +1,2 @@
+var e=document.querySelectorAll(".list-item"),t=document.getElementById("largeImg");e.forEach(function(e){e.addEventListener("click",function(e){e.preventDefault(),t.setAttribute("src",e.currentTarget.children[0].href)})});
+//# sourceMappingURL=index.6b936fc7.js.map
